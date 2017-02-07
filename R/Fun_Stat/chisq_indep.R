7f850d7b54264b89fb3e68657af592c682725be1
Andy Lin <andylin@lotlinx-pc1040.local>
2017-02-07 14:19:57 -0600

    updated chisq_indep.R

--- R/Fun_Stat/chisq_indep.R
+++ R/Fun_Stat/chisq_indep.R
@@ -1,13 +1,15 @@
 chisq_indep <- function(x, alpha = 0.05){
 
-# This is a function to help you calculate the indepence-Chisq test.
-# a input should be a list object
-  #    list(Cabin = c(Yes = 299, No = 280), Steerage = c(Yes = 186, No = 526))
+#  This is a function to help you calculate the indepence-Chisq test.
+#  a input should be a list object
+#  list(Cabin = c(Yes = 299, No = 280), Steerage = c(Yes = 186, No = 526))
   if (!is.list(x)){
     cat('x should be a list object\n')
   }else{
     df <- as.data.frame(x)
-    row <- apply(df, 1, sum); row <- row / sum(row); row <- as.matrix(row, ncol =1)
+    row <- apply(df, 1, sum)
+    row <- row / sum(row)
+    row <- as.matrix(row, ncol =1)
     col <- apply(df,2, sum); col <- as.matrix(col, ncol = 1)
     expected_mat <- row%*%t(col)
     chisq_stat <- sum((df - expected_mat)^2 / expected_mat)
